b8ad5dac9c862f0e587a4a5618316a5f3e26da4b
Tobias Kothbauer <101700683+TobiasKothbauer@users.noreply.github.com>
2024-01-02 22:37:21 +0100

    fetching contact data

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,6 +1,36 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+import { useContactsStore } from '@/stores/useContactStore';
+
+const contacts = useContactsStore();
+
+onBeforeMount(async () => {
+  if (!contacts.contactDetails || !contacts.contactDetails.length) {
+    const url = 'https://randomuser.me/api/?results=5';
+    const res = await fetch(url);
+    const { results } = await res.json();
+
+    if (results && results.length > 0) {
+      const contactsData = results.map((result, index) => ({
+        id: index + 1,
+        firstName: result.name.first,
+        lastName: result.name.last,
+        telNumber: result.phone,
+        email: result.email,
+      }));
+
+      contacts.setContactDetails(contactsData);
+    }
+  }
+});
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Contacts Page</h1>
+    <div v-for="contact in contacts.contactDetails" :key="contact.id">
+      <p>{{ contact.firstName }} {{ contact.lastName }}</p>
+    </div>
   </div>
 </template>
 
