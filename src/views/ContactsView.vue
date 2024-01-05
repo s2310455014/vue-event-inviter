c52d4c126c9c5d763681a56adcf9aabeb1b1d3fe
Tobias Kothbauer <101700683+TobiasKothbauer@users.noreply.github.com>
2024-01-05 14:18:51 +0100

    fetch contacts before app is mounted

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,40 +1,10 @@
 <script setup lang="ts">
-import { computed, onBeforeMount, ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useContactsStore } from '@/stores/useContactStore'
 import ContactCard from '@/components/ContactCard.vue'
-import type { Contact } from '@/types'
 
 const contactStore = useContactsStore()
 const searchTerm = ref('')
-
-onBeforeMount(async () => {
-  if (!contactStore.contacts || !contactStore.contacts.length) {
-    const url = 'https://randomuser.me/api/?results=20'
-    const res = await fetch(url)
-    const { results } = await res.json()
-
-    if (results && results.length > 0) {
-      const contactsData: Contact[] = results.map(
-        (
-          result: { name: { first: string; last: string }; phone: any; email: any },
-          index: number
-        ) => ({
-          id: index + 1,
-          firstName: result.name.first,
-          lastName: result.name.last,
-          name: result.name.first + ' ' + result.name.last,
-          telNumber: result.phone,
-          email: result.email
-        })
-      )
-
-      for (const contact of contactsData) {
-        contactStore.addContact(contact)
-        console.log(contact)
-      }
-    }
-  }
-})
 
 const filteredContacts = computed(() => {
   const searchTermLowerCase = searchTerm.value.toLowerCase()
