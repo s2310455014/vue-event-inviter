d6cf6863a9bbcd19db3a5071c0349b615a3230a0
Tobias Kothbauer <101700683+TobiasKothbauer@users.noreply.github.com>
2024-01-30 22:00:35 +0100

    comment out unused file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+/*
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
@@ -21,3 +22,4 @@ const router = createRouter({
 })
 
 export default router
+*/
