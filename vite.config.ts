fa04ecb2dbdbd5f0c5cb2ff7909d539caec07e5e
Tobias Kothbauer <101700683+TobiasKothbauer@users.noreply.github.com>
2024-01-31 14:36:24 +0100

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/vue-event-inviter/'
+  base: 'https://s2310455014.github.io/vue-event-inviter/'
 })
